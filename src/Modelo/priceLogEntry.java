3707016818633ea8b06311c4f3721954e0d70efb
wess <wess@wess-PC>
2024-04-18 23:37:19 -0600

    Agregue una carpeta para las clases de la parte de modelo y una clase

---
+++ src/Modelo/priceLogEntry.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+import java.time.LocalDate;
+/**
+ *
+ * @author wess
+ */
+
+public class priceLogEntry {
+    private LocalDate date;
+    private long price;
+
+
+public priceLogEntry(int id)
+{
+
+}
+
+}
